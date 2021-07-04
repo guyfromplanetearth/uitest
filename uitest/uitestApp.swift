ee0f8bcd57e7a72ab807e9ec2934a11ebb627978
guyfromplanetearth <guyfromplanetearth@gmail.com>
2021-07-05 02:04:38 +0300

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Nazar Kostiv on 05.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
